a9c5268cc8667bb925dc30b33b3bd7f34ca3dce0
Embersong <evebay@mail.ru>
2024-03-18 13:26:25 +0700

    Add files via upload

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends BaseController
+{
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $success['userName'] = Auth::user()->name;
+        Auth::user()->tokens()->delete();
+
+        return $this->sendResponse($success, 'Logged out successfully');
+    }
+
+    public function signin(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $authUser = Auth::user();
+            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken;
+            $success['name'] =  $authUser->name;
+
+            return $this->sendResponse($success, 'User signed in');
+        }
+        else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+    }
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+
+            return $this->sendError('Error validation', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+
+        return $this->sendResponse($success, 'User created successfully.');
+    }
+}
